e3c172ca053919246fde5ecfb0b5af8be508f249
marlowgate-ops <marlowgate@gmail.com>
2025-09-05 23:06:04 +0900

    Update page.tsx

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import { allPosts } from '@/.contentlayer/generated'
+import { allPosts } from 'contentlayer/generated';
 import { SITE } from '@/lib/site'
 import { PostCard } from '@/components/PostCard'
 
