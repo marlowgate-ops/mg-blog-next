b54114023db25269b6c07db71b93a14344331544
marlowgate-ops <marlowgate@gmail.com>
2025-09-11 07:51:48 +0900

    Update MDXRenderer.tsx

--- components/MDXRenderer.tsx
+++ components/MDXRenderer.tsx
@@ -1,6 +1,4 @@
-// app/components/MDXRenderer.tsx
 'use client'
-import React from 'react'
 import { useMDXComponent } from 'next-contentlayer/hooks'
 
 export default function MDXRenderer({ code }: { code: string }) {
