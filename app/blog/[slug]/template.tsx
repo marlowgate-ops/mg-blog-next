a6e1c102a134540371b1a6f99d0258245d52dd77
marlowgate-ops <marlowgate@gmail.com>
2025-09-13 16:04:13 +0900

    Update template.tsx

--- app/blog/[slug]/template.tsx
+++ app/blog/[slug]/template.tsx
@@ -1,5 +1,13 @@
 import type { ReactNode } from 'react'
 import ArticleCTA from '../../../components/ArticleCTA'
+import JsonLdArticle from '../../../components/JsonLdArticle'
+
 export default function BlogPostTemplate({ children }: { children: ReactNode }) {
-  return (<>{children}<ArticleCTA /></>)
+  return (
+    <>
+      <JsonLdArticle />
+      {children}
+      <ArticleCTA />
+    </>
+  )
 }
