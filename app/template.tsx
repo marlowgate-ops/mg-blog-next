d86dde09afb4259fb7634c1f540206d299faecdf
marlowgate-ops <marlowgate@gmail.com>
2025-09-13 16:03:47 +0900

    Update template.tsx

--- app/template.tsx
+++ app/template.tsx
@@ -1,5 +1,15 @@
 import type { ReactNode } from 'react'
 import FooterMini from '../components/FooterMini'
+import JsonLdBreadcrumbs from '../components/JsonLdBreadcrumbs'
+import GA4CopyVariant from '../components/GA4CopyVariant'
+
 export default function Template({ children }: { children: ReactNode }) {
-  return (<>{children}<FooterMini /></>)
+  return (
+    <>
+      <GA4CopyVariant />
+      <JsonLdBreadcrumbs />
+      {children}
+      <FooterMini />
+    </>
+  )
 }
