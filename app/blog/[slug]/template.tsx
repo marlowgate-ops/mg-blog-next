12d3fa12dc35862341c9c5ae482f7adfbc600b32
marlowgate-ops <marlowgate@gmail.com>
2025-09-13 15:50:01 +0900

    Update template.tsx

--- app/blog/[slug]/template.tsx
+++ app/blog/[slug]/template.tsx
@@ -1,7 +1,5 @@
 import type { ReactNode } from 'react'
-import ArticleCTA from '@/components/ArticleCTA'
-
-// Inject CTA right after each article page without changing page.tsx
+import ArticleCTA from '../../../components/ArticleCTA'
 export default function BlogPostTemplate({ children }: { children: ReactNode }) {
   return (<>{children}<ArticleCTA /></>)
 }
