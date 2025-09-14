8eee81eab94fd632ae904993bbe2372b0a7b0bcb
marlowgate-ops <marlowgate@gmail.com>
2025-09-14 15:24:16 +0900

    Update layout.tsx

--- app/blog/[slug]/layout.tsx
+++ app/blog/[slug]/layout.tsx
@@ -1,9 +1,11 @@
 import type { ReactNode } from 'react'
+import Breadcrumbs from '../../../components/Breadcrumbs'
 import EndCTA from '../../../components/EndCTA'
 
 export default function BlogPostLayout({ children }: { children: ReactNode }) {
   return (
     <article>
+      <Breadcrumbs />
       {children}
       <EndCTA />
     </article>
