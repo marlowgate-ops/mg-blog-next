66821548eadda687b5e53a547d78eaaba84fd9c8
marlowgate-ops <marlowgate@gmail.com>
2025-09-17 14:01:18 +0900

    Update JsonLd.tsx

--- components/JsonLd.tsx
+++ components/JsonLd.tsx
@@ -1,12 +1,4 @@
 import React from "react";
-type Props = { data: any };
-/** Renders JSON-LD safely for Next.js App Router */
-export default function JsonLd({ data }: Props) {
-  return (
-    <script
-      type="application/ld+json"
-      // We stringify with safe fallback to avoid "undefined"
-      dangerouslySetInnerHTML={{ __html: JSON.stringify(data ?? {}) }}
-    />
-  );
+export default function JsonLd({ data }: { data: any }) {
+  return <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(data ?? {}) }} />;
 }
